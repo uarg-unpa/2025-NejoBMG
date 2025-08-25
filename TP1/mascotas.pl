d6878f6cd25a2dacd2c757aee77f611824ef8c2b
NejoBMG <104396269+NejoBMG@users.noreply.github.com>
2025-08-25 22:29:37 +0000

    Primer avance TP1

---
+++ TP1/mascotas.pl
@@ -0,0 +1,12 @@
+
+mascota(perro,fido).
+mascota(gato,whiskers).
+mascota(perro,rex).
+
+duenio(juan,fido).
+duenio(ana,whiskers).
+duenio(pedro,rex).
+
+color(fido,marron).
+color(whiskers,gris).
+color(rex,negro).
---
+++ TP1/punto1.pl
@@ -0,0 +1,6 @@
+hermano(juan,julia).
+valiente(jose).
+lee(pedro,hamlet).
+compuso(mozart,la_flauta_magica).
+visito(pablo,norma,holanda).
+alta(mirta).
